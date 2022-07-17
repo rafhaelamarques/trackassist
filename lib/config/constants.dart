074418ce18208a709ca332471f0130e2a02540fa
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2022-07-17 19:42:56 -0300

    primeiras telas

---
+++ lib/config/constants.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String APP_NAME = "Track Assist";
+
+const String SERVER_URL = '';
+
+// TEXTOS, TAMANHOS E CORES
+const double TAMANHO_TEXTO_NOME = 48;
+
+const Color COR_PRIMARIA = Color.fromRGBO(144, 0, 0, 1);
+const Color COR_SECUNDARIA = Color.fromRGBO(217, 217, 217, 1);
+const Color COR_TERCIARIA = Color.fromRGBO(249, 177, 37, 1);
+const Color FORM_COLOR = Color.fromRGBO(116, 119, 124, 1);
+const Color SOFT_GREY = Color(0xFFaaaaaa);
+const Color GREY = Colors.grey;
+const Color BLACK_GREY = Color(0xff777777);
+const Color BLACK21 = Colors.black;
+const Color BLACK55 = Color(0xFF555555);
+
+const int STATUS_OK = 200;
+const int STATUS_BAD_REQUEST = 400;
+const int STATUS_NOT_AUTHORIZED = 403;
+const int STATUS_NOT_FOUND = 404;
+const int STATUS_INTERNAL_ERROR = 500;
+
+// ERROS
+const String ERROR_OCCURED = 'Um erro ocorreu, tente novamente mais tarde.';
--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:trackassist/config/constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,12 +15,187 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Track Assist'),
-      ),
-      body: Center(
-        child: const Text('Home Page'),
-      ),
-    );
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text('TrackAssist'),
+          backgroundColor: COR_PRIMARIA,
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 150,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25))),
+                      backgroundColor: MaterialStateColor.resolveWith(
+                        ((states) => COR_PRIMARIA),
+                      ),
+                      elevation: MaterialStateProperty.resolveWith<double>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.disabled)) {
+                            return 0;
+                          }
+                          return 0;
+                        },
+                      ),
+                      side: MaterialStateBorderSide.resolveWith(
+                          (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.hovered) ||
+                            states.contains(MaterialState.selected)) {
+                          return const BorderSide(
+                              color: Colors.white, width: 2);
+                        }
+                        return const BorderSide(color: COR_PRIMARIA, width: 2);
+                      }),
+                    ),
+                    child: const Text('Início'),
+                  ),
+                ),
+                const SizedBox(width: 80),
+                Container(
+                  width: 150,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25))),
+                      backgroundColor: MaterialStateColor.resolveWith(
+                        ((states) => COR_PRIMARIA),
+                      ),
+                      elevation: MaterialStateProperty.resolveWith<double>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.disabled)) {
+                            return 0;
+                          }
+                          return 0;
+                        },
+                      ),
+                      side: MaterialStateBorderSide.resolveWith(
+                          (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.hovered) ||
+                            states.contains(MaterialState.selected)) {
+                          return const BorderSide(
+                              color: Colors.white, width: 2);
+                        }
+                        return const BorderSide(color: COR_PRIMARIA, width: 2);
+                      }),
+                    ),
+                    child: const Text('Prestadores'),
+                  ),
+                ),
+                const SizedBox(width: 80),
+                Container(
+                  width: 150,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25))),
+                      backgroundColor: MaterialStateColor.resolveWith(
+                        ((states) => COR_PRIMARIA),
+                      ),
+                      elevation: MaterialStateProperty.resolveWith<double>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.disabled)) {
+                            return 0;
+                          }
+                          return 0;
+                        },
+                      ),
+                      side: MaterialStateBorderSide.resolveWith(
+                          (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.hovered) ||
+                            states.contains(MaterialState.selected)) {
+                          return const BorderSide(
+                              color: Colors.white, width: 2);
+                        }
+                        return const BorderSide(color: COR_PRIMARIA, width: 2);
+                      }),
+                    ),
+                    child: const Text('Histórico'),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Flexible(
+                    child: ListView(
+                      itemExtent: 95,
+                      children: [
+                        ListTile(
+                          title: const Icon(
+                            Icons.location_on_outlined,
+                            size: 40,
+                            color: COR_PRIMARIA,
+                          ),
+                          onTap: () {},
+                        ),
+                        ListTile(
+                          title: const Icon(
+                            Icons.search,
+                            size: 40,
+                            color: COR_PRIMARIA,
+                          ),
+                          onTap: () {},
+                        ),
+                        ListTile(
+                          title: const Icon(
+                            Icons.map,
+                            size: 40,
+                            color: COR_PRIMARIA,
+                          ),
+                          onTap: () {},
+                        ),
+                        ListTile(
+                          title: const Icon(
+                            Icons.palette,
+                            size: 40,
+                            color: COR_PRIMARIA,
+                          ),
+                          onTap: () {},
+                        ),
+                        ListTile(
+                          title: const Icon(
+                            Icons.filter_list_alt,
+                            size: 40,
+                            color: COR_PRIMARIA,
+                          ),
+                          onTap: () {},
+                        ),
+                        ListTile(
+                          title: const Icon(
+                            Icons.info,
+                            size: 40,
+                            color: COR_PRIMARIA,
+                          ),
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    height: double.maxFinite,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
